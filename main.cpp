092569cbcf0c969dfb4272e74ba41e976e1a94c4
ZKlack <xklack@outlook.sa>
2023-02-08 19:20:32 +0300

    final
    in this commit the main function will generate 2^14 arrays with incrementing sizes starting from 1 and then run each sort function thru every array recording the speed of each sort and storing all of that information in a simple text file

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 #include<map>
 #include<ctime>
 #include<chrono>
+#include<fstream>
+#include<filesystem>
 
 template<class T> long long speedTest(void(*)(T*,T*,bool(*)(T&,T&)),std::vector<T>&,bool(*)(T&,T&) = zk::sorting::comparasions::less);
 template<class T> bool sortTest(void(*)(T*,T*,bool(*)(T&,T&)),std::vector<T>&,bool(*)(T&,T&) = zk::sorting::comparasions::less);
+template<class T> bool isSorted(std::vector<T>&,bool(*)(T&,T&) = zk::sorting::comparasions::less);
 
 int main()
 {
+    std::cout<<"program started\n";
     srand(time(NULL));
     std::vector<std::pair<std::string,void(*)(int*,int*,bool(*)(int&,int&))> > functions{
             {"insertion sort",                  zk::sorting::insertion},
@@ -22,13 +26,63 @@ int main()
             {"quick sort (middle element)",     zk::sorting::quick_middle},
             {"quick sort (random element)",     zk::sorting::quick_random}
     };
-    std::vector<int> Arr(1<<12);
-    for(auto &i:Arr)
-        i=rand();
-    for(int i=0;i<functions.size();++i)
+    if(std::filesystem::exists("random_arrays.txt"))
+        std::filesystem::remove("random_arrays.txt");
+    std::ofstream fout("random_arrays.txt",std::fstream::out);
+    size_t maxsize=1<<14,minsize=1,samplesize=maxsize-minsize+1;
+    fout<<samplesize<<'\n';
+    for(size_t i=minsize;i<=maxsize;++i)
     {
-        std::vector<int> arr(Arr);
-        std::cout<<functions[i].first<<": "<<speedTest(functions[i].second,arr)<<" microseconds\n";
+        fout<<i<<'\n';
+        for(size_t j=0;j<i;++j)
+            fout<<rand()<<' ';
+        fout<<'\n';
+    }
+    fout.close();
+    for(const auto &function:functions)
+    {
+        if(!std::filesystem::is_directory(function.first))
+            std::filesystem::create_directory(function.first);
+        if(std::filesystem::exists(function.first+"\\sorted_arrays.txt"))
+            std::filesystem::remove(function.first+"\\sorted_arrays.txt");
+        if(std::filesystem::exists(function.first+"\\speeds.txt"))
+            std::filesystem::remove(function.first+"\\speeds.txt");
+        std::cout<<"started "<<function.first;
+        std::ofstream arrays(function.first+"\\sorted_arrays.txt",std::fstream::out);
+        std::ofstream speeds(function.first+"\\speeds.txt",std::fstream::out);
+        std::ifstream fin("random_arrays.txt",std::fstream::in);
+        size_t enteries;
+        fin>>enteries;
+        arrays<<enteries<<'\n';
+        speeds<<enteries<<'\n';
+        while(enteries--)
+        {
+            size_t size;
+            fin>>size;
+            arrays<<size<<'\n';
+            speeds<<size<<' ';
+            std::vector<int> arr(size);
+            for(size_t i=0;i<size;++i)
+                fin>>arr[i];
+            unsigned long long speed=speedTest(function.second,arr);
+            speeds<<speed<<' ';
+            if(!isSorted(arr))
+            {
+                std::cout<<"\narray not sorted:\n\t-sort: "<<function.first<<"\n\t-size: "<<size;
+                return 1;
+            }
+            speed=speedTest(function.second,arr);
+            speeds<<speed<<'\n';
+            for(size_t i=0;i<size;++i)
+                arrays<<arr[i]<<' ';
+            arrays<<'\n';
+            if(enteries%500==0)
+                std::cout<<"\n\t"<<enteries<<" enteries left...";
+        }
+        arrays.close();
+        speeds.close();
+        fin.close();
+        std::cout<<"\nDone!\n";
     }
 }
 
@@ -51,7 +105,18 @@ template<class T> bool sortTest(
         bool(*comp)(T&,T&)
                 )
 {
-    algo(arr.data(),arr.data()+arr.size(),zk::sorting::comparasions::less);
+    algo(arr.data(),arr.data()+arr.size(),comp);
+    bool sorted=true;
+    for(size_t i=1;i<arr.size()&&sorted;++i)
+        sorted&=!comp(arr[i],arr[i-1]);
+    return sorted;
+}
+
+template<class T> bool isSorted(
+        std::vector<T>& arr,
+        bool(*comp)(T&,T&)
+)
+{
     bool sorted=true;
     for(size_t i=1;i<arr.size()&&sorted;++i)
         sorted&=!comp(arr[i],arr[i-1]);
